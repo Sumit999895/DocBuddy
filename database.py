2398fa819ce16ff6d12cb1c9039e834a0b82d3e6
Your Name <sk999895@gmail.com>
2026-09-10 23:35:41 +0530

    Configure Aiven MySQL connection

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import mysql.connector
 
 from config import (
     DB_HOST,
+    DB_PORT,
     DB_USER,
     DB_PASSWORD,
     DB_NAME
@@ -9,10 +10,12 @@ from config import (
 
 
 def get_db_connection():
-
     return mysql.connector.connect(
         host=DB_HOST,
+        port=DB_PORT,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
+        ssl_disabled=False,
+        ssl_verify_cert=False
     )
